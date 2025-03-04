d4ee171c2297c58119c2ab881d6d4706908b757d
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-03-04 08:30:48 +0530

    Added Comments

--- ball.py
+++ ball.py
@@ -6,23 +6,21 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.x_move=10
-        self.y_move=10
+        self.x_move = 10
+        self.y_move = 10
         self.move_speed = 0.1
 
     def ball_move(self):
-        self.goto(x=(self.xcor()+self.x_move),y=(self.ycor()+self.y_move))
+        self.goto(x=(self.xcor() + self.x_move), y=(self.ycor() + self.y_move))
 
     def bounce_y(self):
-       self.y_move *= -1
+        self.y_move *= -1
 
     def bounce_x(self):
         self.x_move *= -1
-        self.move_speed*=0.9
-
+        self.move_speed *= 0.9
 
     def reset_position(self):
-        self.goto(0,0)
+        self.goto(0, 0)
         self.bounce_x()
-        self.move_speed=0.1
-
+        self.move_speed = 0.1
--- main.py
+++ main.py
@@ -4,50 +4,50 @@ from ball import Ball
 import time
 from scoreboard import Score
 
+# Set up the screen
 screen = Screen()
-paddle_left = Paddle(-350,0)
-paddle_right = Paddle(350,0)
+paddle_left = Paddle(-350, 0)
+paddle_right = Paddle(350, 0)
 ball = Ball()
-score=Score()
-
+score = Score()
 
 screen.title("The Pong Game")
 screen.bgcolor("black")
-screen.setup(800,610)
+screen.setup(800, 610)
 screen.tracer(0)
 
-#For Animation
+# Listen for key presses
 screen.listen()
-
-screen.onkey(paddle_right.go_up,"Up")
-screen.onkey(paddle_right.go_down,"Down")
-
-screen.onkey(paddle_left.go_up,"w")
-screen.onkey(paddle_left.go_down,"s")
+screen.onkey(paddle_right.go_up, "Up")
+screen.onkey(paddle_right.go_down, "Down")
+screen.onkey(paddle_left.go_up, "w")
+screen.onkey(paddle_left.go_down, "s")
 
 game_is_on = True
 
+# Main game loop
 while game_is_on:
     time.sleep(ball.move_speed)
     screen.update()
     ball.ball_move()
     score.update()
 
-    if ball.ycor()==290 or ball.ycor()==-290:
+    # Detect collision with top and bottom walls
+    if ball.ycor() == 290 or ball.ycor() == -290:
         ball.bounce_y()
 
-    #Detect Collision with r_paddle
-    if (ball.distance(paddle_right) < 50 and ball.xcor()>330 or
-            ball.distance(paddle_left) < 50 and ball.xcor()<-330):
-
+    # Detect collision with paddles
+    if (ball.distance(paddle_right) < 50 and ball.xcor() > 330 or
+            ball.distance(paddle_left) < 50 and ball.xcor() < -330):
         ball.bounce_x()
 
-
-    if ball.xcor()>380:
+    # Detect when right paddle misses
+    if ball.xcor() > 380:
         ball.reset_position()
         score.l_point()
 
-    if  ball.xcor()<-380:
+    # Detect when left paddle misses
+    if ball.xcor() < -380:
         ball.reset_position()
         score.r_point()
 
